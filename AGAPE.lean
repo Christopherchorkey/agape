26a5aabbc80b8e8097c827c3439d1e06eda37636
Christopherchorkey <chris.chorkey@gmail.com>
2026-08-17 20:22:59 -0700

    Add files via upload

---
+++ AGAPE.lean
@@ -0,0 +1,11 @@
+import AGAPE.Core
+import AGAPE.Energy
+import AGAPE.Logistics
+import AGAPE.Plasticity
+import AGAPE.Creativity
+
+/-! # AGAPE Framework
+Adaptive Generative Architecture for Plasticity and Evolution
+
+This is the main module that imports all components of the AGAPE framework.
+-/
